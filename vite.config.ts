007b4c4bbf0260a3f74ab6f023e2cf7217db0b3a
devchaeyoung <chaemaa00@gmail.com>
2024-05-14 09:24:05 +0900

    feat: pwa 설정 추가

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.png"],
+      injectRegister: "script",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
