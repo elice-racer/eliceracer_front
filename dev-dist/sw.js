3839d31baa8e84c12c7a81052904787c85742fde
devchaeyoung <chaemaa00@gmail.com>
2024-06-14 20:16:45 +0900

    feat: 회원가입시 비밀번호, 전화번호 정규식 추가

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4o232l7l5b"
+    "revision": "0.lm2apv7e2gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
