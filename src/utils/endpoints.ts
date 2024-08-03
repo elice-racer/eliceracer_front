15cc77c2cac40d89e5a68b0b9c083d334e86280e
devchaeyoung <chaemaa00@gmail.com>
2024-08-03 23:31:43 +0900

    Docs: 요청 url 별 상세설명 추가

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,20 +1,27 @@
 const ADMINS = {
+  /** @상세 admins/users/:userId */
   users: "admins/users",
   membersCoaches: "admins/members/coaches",
   membersRacers: "admins/members/racers",
-
+  /** @상세 admins/users/roles/:userId */
   usersRole: "admins/users/roles",
+  /** @상세 admins/users/tracks/:userId */
   usersTrack: "admins/users/tracks",
 
+  /** @상세 admins/teams/:teamId */
   teams: "admins/teams",
   tracks: "admins/tracks",
-
+  /** @상세 admins/projects/:projectId */
   projects: "admins/projects",
-
+  /**
+   * @상세 admins/chats/:trackId
+   * @쿼리 admins/chats?tracks=&cardinalNo=&round&type=
+   *  */
   chats: "admins/chats",
   teamsChats: "admins/chats/teams",
+  /** @상세 admins/officehours/:projectId */
   officehour: "admins/officehours",
-
+  /** @쿼리 admins/verify-email?id=&token= */
   verifyEmail: "admins/verify-email",
   signup: "admins/signup",
 };
@@ -32,26 +39,32 @@ const AUTH = {
 };
 
 const CHATS = {
+  /** @상세 chats/:chatId */
   chats: "chats",
+  /** @쿼리 chats/messages?chatId=&pageSize= */
   chatsMessages: "chats/messages",
   chatRooms: "chats/rooms/all",
+  /** @상세 chats/users/:chatId */
   chatUsers: "chats/users",
 };
 
 const NOTICES = {
+  /** @상세 admins/notices/:noticeId */
   notices: "admins/notices",
+  /** @상세 notices/:noticeId */
   notice: "notices",
   AllNotices: "notices/all",
+  /** @상세 notices/authors?userId=&page=&pageSize= */
   authorsNotice: "notices/authors",
 };
 
 const OFFICE_HOURS = {
   /**
    * @상세 officehours/:officehourId
-   * @단순조회 officehours?tracks=&cardinalNo=&round=&teamNo=
+   * @쿼리 officehours?tracks=&cardinalNo=&round=&teamNo=
    */
   officehour: "officehours",
-  /** @유저아이디로_검색 officehours/coach/:userId */
+  /** @상세 officehours/coach/:userId */
   coachsOfficehour: "officehours/coach",
   /** @상세 officehours/project/:projectId */
   projectsOfficehour: "officehours/project",
@@ -92,13 +105,30 @@ const TRACKS = {
   /**
    * @쿼리 tracks/search&search=
    */
-  searchTracks: "tracks/search",
+  search: "tracks/search",
 };
 
 const USERS = {
   /**
-   * @
+   * @상세 users/:userId
    */
+  users: "users",
+  current: "users/current",
+  /** @상세 users/miniprofiles/:userId */
+  miniProfile: "users/miniprofile",
+  mypage: "users/mypage",
+  profileImg: "users/profileImages",
+  /** @쿼리 users/search?realName= */
+  search: "users/search",
+  /** @쿼리 users/skills?skillName= */
+  skills: "users/skills",
+  phoneNumber: "users/mypage/phoneNumber",
+  signup: "users/signup",
 };
 
-export { ADMINS, AUTH, CHATS, NOTICES, OFFICE_HOURS, PROJECTS, TEAMS, TRACKS, USERS };
+const APP_PUSH = {
+  /** @fcm fcm 토큰*/
+  fcmNotifications: "notifications/tokens",
+};
+
+export { ADMINS, AUTH, CHATS, NOTICES, OFFICE_HOURS, PROJECTS, TEAMS, TRACKS, USERS, APP_PUSH };
