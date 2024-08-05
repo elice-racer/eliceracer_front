84a1f092253aa51d8691618a736d6adaebe95c51
devchaeyoung <chaemaa00@gmail.com>
2024-08-05 12:24:50 +0900

    feat: useAxios추가

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -1,49 +1,88 @@
-import { useEffect, useState } from "react";
-import { instance as axiosInstance } from "../services/instance";
+import { useState } from "react";
 
-interface FetchDataProps {
-  url: string;
-  method: "get" | "post" | "patch" | "put" | "delete";
-  data: {} | undefined;
-  params: {} | undefined;
-}
 const useAxios = () => {
-  let response = null;
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
-  let controller = new AbortController();
-
-  useEffect(() => {
-    return () => controller?.abort();
-  }, []);
-
-  const fetchData = async ({ url, method, data = {}, params = {} }: FetchDataProps) => {
+  const fetchData = async (callApi: any) => {
     setLoading(true);
 
-    controller.abort();
-    controller = new AbortController();
     try {
-      response = await axiosInstance({
-        url: url,
-        method: method,
-        data: data,
-        params,
-        signal: controller.signal,
-      });
-      return response;
+      const res = await callApi();
+      return res;
     } catch (e: any) {
-      //   if (axiosInstance.isCancel(e)) {
-      //     console.error("Request cancelled", e.message);
-      //   } else {
-      //     setError(e.response ? e.response.data : e.message);
-      //   }
       setError(e.response ? e.response.data : e.message);
     } finally {
       setLoading(false);
     }
   };
 
-  return { response, error, loading, fetchData };
+  return { error, loading, fetchData };
 };
+
 export default useAxios;
+
+// const useAxios= () => {
+//   const [error, setError] = useState<string>("");
+//   const [loading, setLoading] = useState<boolean>(false);
+
+//   let controller = new AbortController();
+
+//   useEffect(() => {
+//     return () => controller?.abort();
+//   }, []);
+
+//   const fetchData = async (url: string, method: AxiosMethod, data?: any, params?: any) => {
+//     setLoading(true);
+
+//     controller.abort();
+//     controller = new AbortController();
+//     try {
+//       switch (method) {
+//         case "get":
+//           return await axiosInstance.get(url);
+//         case "post":
+//           return await axiosInstance.post(url, data);
+//         case "patch":
+//           return await axiosInstance.patch(url, data);
+//         case "delete":
+//           return await axiosInstance.delete(url, data);
+//       }
+//     } catch (e: any) {
+//       setError(e.response ? e.response.data : e.message);
+//     } finally {
+//       setLoading(false);
+//     }
+
+//     try {
+//       const response = await axiosInstance({
+//         url: url,
+//         method: method,
+//         data: data,
+//         params,
+//         signal: controller.signal,
+//       });
+
+//       return response;
+//     } catch (e: any) {
+//     } finally {
+//     }
+//   };
+
+//   return { error, loading, fetchData };
+// };
+
+// const MyCoponent = () => {
+//   const { fetchData, loading, error } = useAxios();
+//   const [data, setData] = useState<any>();
+
+//   const fetchUserPage = async () => {
+//     const res = await fetchData("getUser", "get", {}, {});
+//     setData(res);
+//   };
+
+//   useEffect(() => {
+//     fetchUserPage();
+//   }, []);
+//   return <div>유저정보</div>;
+// };
